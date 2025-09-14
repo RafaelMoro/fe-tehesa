f4826b45387e69aa4bfe762e18517393653878b8
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-14 12:48:01 -0600

    Add Apollo Client setup and update Home component for GraphQL queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+import createApolloClient from "./apollo-client";
+import { gql } from "@apollo/client";
+
 import { TestComp } from "@/components/TestComp";
 
-export default function Home() {
+export default async function Home() {
+  const client = createApolloClient();
+  const { data } = await client.query({
+    query: gql`
+      query ExampleQuery {
+        products {
+          available
+          category
+          name
+          image {
+            url
+          }
+        }
+      }
+    `,
+  });
+  console.log('datta', data)
   return (
     <div>
       <TestComp />
