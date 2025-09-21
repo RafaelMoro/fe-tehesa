a82be88446b3aa5d35a61f19bc319508e965def9
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-21 10:06:41 -0600

    Add next-themes for dark mode support and update ToggleDarkMode component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="es" className="dark text-foreground bg-background">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          {children}
+          <NextThemesProvider attribute="class" defaultTheme="dark">
+            {children}
+          </NextThemesProvider>
         </Providers>
       </body>
     </html>
--- src/shared/ui/atoms/ToggleDarkMode.tsx
+++ src/shared/ui/atoms/ToggleDarkMode.tsx
@@ -1,10 +1,25 @@
 "use client"
 import { Button } from "@heroui/react"
 import { RiMoonLine } from "@remixicon/react"
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export const ToggleDarkMode = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  const toggleDarkMode = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
   return (
-    <Button isIconOnly>
+    <Button isIconOnly onClick={toggleDarkMode}>
       <RiMoonLine />
     </Button>
   )
