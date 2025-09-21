6f5310b8f94eb7ab34c51e1ce18444ce3e7bd435
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-21 12:38:17 -0600

    Add useMediaQuery hook for responsive design handling in ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,12 +1,14 @@
 "use client"
+import { useMediaQuery } from "@/shared/hooks/useMediaQuery"
 import { Product } from "@/shared/types/global.types"
-import { Card, CardHeader, Image } from "@heroui/react"
+import { Card, CardBody, CardHeader, Image } from "@heroui/react"
 
 interface ProductCardProps {
   product: Product
 }
 
 export const ProductCard = ({ product }: ProductCardProps) => {
+  const { isMobile } = useMediaQuery()
   const categoryLabels: Record<string, string> = {
     'twist_drill_bits': 'Brocas',
     'shockwave_impact_adapter': 'Broquero',
@@ -15,19 +17,40 @@ export const ProductCard = ({ product }: ProductCardProps) => {
 
   return (
     <Card>
-      <CardHeader className="flex gap-3">
-        <Image
-          alt={product.name}
-          height={200}
-          width={200}
-          radius="sm"
-          src={`http://localhost:1337${product.image.url}`}
-        />
-        <div className="flex flex-col">
-          <h5 className="text-2xl font-bold">{product.name}</h5>
-          <p className="text-gray-400">{categoryLabels[product.category] ?? product.category}</p>
-        </div>
-      </CardHeader>
+      { isMobile && (
+        <>
+          <CardHeader className="flex justify-center">
+              <Image
+                alt={product.name}
+                height={200}
+                width={200}
+                radius="sm"
+                src={`http://localhost:1337${product.image.url}`}
+              />
+          </CardHeader>
+          <CardBody>
+            <div className="flex flex-col">
+              <h5 className="text-2xl font-bold">{product.name}</h5>
+              <p className="text-gray-400">{categoryLabels[product.category] ?? product.category}</p>
+            </div>
+          </CardBody>
+        </>
+      ) }
+      { !isMobile && (
+        <CardHeader className="flex gap-3">
+          <Image
+            alt={product.name}
+            height={200}
+            width={200}
+            radius="sm"
+            src={`http://localhost:1337${product.image.url}`}
+          />
+          <div className="flex flex-col">
+            <h5 className="text-2xl font-bold">{product.name}</h5>
+            <p className="text-gray-400">{categoryLabels[product.category] ?? product.category}</p>
+          </div>
+        </CardHeader>
+      )}
     </Card>
   )
 }
