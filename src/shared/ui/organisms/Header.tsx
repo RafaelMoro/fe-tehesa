605b03983b7c78aeedfbf29ddee14023d1611a5f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-21 11:36:07 -0600

    Refactor logo rendering logic in Header component for improved readability

--- src/shared/ui/organisms/Header.tsx
+++ src/shared/ui/organisms/Header.tsx
@@ -8,17 +8,25 @@ interface HeaderProps {
 }
 
 export const Header = ({ themeFetched }: HeaderProps) => {
-  const logoSrc = themeFetched === "light" ? "/tehesa-logo.webp" : "/tehesa-logo-negativo.webp";
-
   return (
     <header className="flex justify-between p-4">
-      <Image
-        alt="Tehesa Logo"
-        height={61}
-        width={115}
-        radius="sm"
-        src={logoSrc}
-      />
+      { themeFetched === 'light' ? (
+        <Image
+          alt="Tehesa Logo"
+          height={61}
+          width={115}
+          radius="sm"
+          src="/tehesa-logo.webp"
+        />
+      ): (
+        <Image
+          alt="Tehesa Logo"
+          height={61}
+          width={115}
+          radius="sm"
+          src="/tehesa-logo-negativo.webp"
+        />
+      )}
       <ToggleDarkMode />
     </header>
   )
