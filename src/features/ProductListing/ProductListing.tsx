5de31b617f505b763b53b4b6e44ef8f32300b071
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-09-21 11:58:54 -0600

    Update ProductListing component to improve responsiveness of product grid layout

--- src/features/ProductListing/ProductListing.tsx
+++ src/features/ProductListing/ProductListing.tsx
@@ -13,7 +13,7 @@ export const ProductListing = ({ products }: ProductListingProps) => {
   }
 
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
       { products.map((product) => (
         <ProductCard key={product.name} product={product} />
       ))}
