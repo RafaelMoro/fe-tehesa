93de14b36ab3536800fd83ab57a04b72141939cc
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-05-07 23:27:26 -0600

    Refactor apollo-client to destructure STRAPI_API_TOKEN from process.env

--- src/app/apollo-client.ts
+++ src/app/apollo-client.ts
@@ -1,12 +1,12 @@
 import { HttpLink } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const { STRAPI_HOST } = process.env
+const { STRAPI_HOST, STRAPI_API_TOKEN } = process.env
 
 const createApolloClient = () => {
   return new ApolloClient({
     link: new HttpLink({ uri: STRAPI_HOST, headers: {
-      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+      Authorization: `Bearer ${STRAPI_API_TOKEN}`,
     } }),
     cache: new InMemoryCache(),
   });
