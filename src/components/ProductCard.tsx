0f33111107eff0384987056f3c34918691be0b4a
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-05-07 22:01:50 -0600

    Refactor ProductCard component layout and enhance variant count display

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useMemo } from "react"
 import { Card, CardBody, CardHeader, Image, Chip } from "@heroui/react"
+import clsx from "clsx"
 
 import { useMediaQuery } from "@/shared/hooks/useMediaQuery"
 import { Product } from "@/shared/types/global.types"
@@ -25,75 +26,62 @@ export const ProductCard = ({ product }: ProductCardProps) => {
     return formatNumberToCurrency(product.maxPrice)
   }, [product.maxPrice])
 
+  const cardHeaderCss = clsx(
+    { "flex justify-center": isMobile },
+    { "flex gap-3": !isMobile }
+  )
+  const titleCSS = clsx(
+    "font-bold",
+    { "text-2xl": !isMobile },
+    { "text-xl": isMobile }
+  )
+
 
   return (
     <Card>
-      { isMobile && (
-        <>
-          <CardHeader className="flex justify-center">
-              {/* <Image
-                alt={product.name}
-                height={200}
-                width={200}
-                radius="sm"
-                src={`http://localhost:1337${product.image.url}`}
-              /> */}
-          </CardHeader>
-          <CardBody>
-            <div className="flex flex-col">
-              <div className="flex flex-col gap-2">
-                <span className="text-gray-400 text-sm">{product.category.name} | {product.brand.name}</span>
-                <h5 className="text-2xl font-bold">{product.name}</h5>
-              </div>
-              { product?.category?.name && (
-                <p className="text-gray-400">{product.category.name}</p>
-              )}
-              { (minPriceString && maxPriceString) && (
-                <p className="text-white text-sm">Desde {minPriceString} hasta {maxPriceString}</p>
-              )}
-              { product?.variantCount && (
-                <p className="text-white text-sm">{product.variantCount} variantes disponibles</p>
-              )}
-            </div>
-          </CardBody>
-        </>
-      ) }
-      { !isMobile && (
-        <CardHeader className="flex gap-3">
-          {/* <Image
+      <CardHeader className={cardHeaderCss}>
+        {/* <Image // IMAGE COMP MOBILE
             alt={product.name}
             height={200}
             width={200}
             radius="sm"
             src={`http://localhost:1337${product.image.url}`}
           /> */}
-          <div className="flex flex-col gap-8">
-            <div className="flex flex-col gap-2">
-              <div className="inline-flex gap-1 text-gray-400">
-                <RiBookmarkLine size={18} />
-                <span className="text-sm">
-                  {product.category?.name} {brandName && `| ${brandName}`}
-                </span>
-              </div>
-              <h5 className="text-2xl font-bold">{product.name}</h5>
-            </div>
-            { product?.variantCount && (
-               <Chip size="sm">
-                <div className="inline-flex gap-2">
-                  <RiStackLine size={18} />
-                  {product.variantCount} variantes disponibles
-                </div>
-              </Chip>
-            )}
-            { (minPriceString && maxPriceString) && (
-              <div className="flex gap-1 text-gray-400">
-                <RiPriceTag3Line size={22} />
-                <p>Desde <span className="font-bold text-xl text-gray-950 dark:text-gray-100">{minPriceString}</span> hasta {maxPriceString}</p>
-              </div>
-            )}
+        {/* <Image // IMAGE COMP DESKTOP
+          alt={product.name}
+          height={200}
+          width={200}
+          radius="sm"
+          src={`http://localhost:1337${product.image.url}`}
+        /> */}
+        <div className="flex flex-col gap-2">
+          <div className="inline-flex gap-1 text-gray-400">
+            <RiBookmarkLine size={18} />
+            <span className="text-sm">
+              {product.category?.name} {brandName && `| ${brandName}`}
+            </span>
           </div>
-        </CardHeader>
-      )}
+          <h5 className={titleCSS}>{product.name}</h5>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <div className="flex flex-col gap-6">
+          { product?.variantCount && (
+            <Chip size="sm">
+              <div className="inline-flex gap-2">
+                <RiStackLine size={18} />
+                {product.variantCount} variantes disponibles
+              </div>
+            </Chip>
+          )}
+          { (minPriceString && maxPriceString) && (
+            <div className="flex gap-1 text-gray-400">
+              <RiPriceTag3Line size={22} />
+              <p>Desde <span className="font-bold text-xl text-gray-950 dark:text-gray-100">{minPriceString}</span> hasta {maxPriceString}</p>
+            </div>
+          )}
+        </div>
+      </CardBody>
     </Card>
   )
 }
