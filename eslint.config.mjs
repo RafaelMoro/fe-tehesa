f0c70c30533021b10958941f248c937cf60498ea
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-07-12 08:01:43 -0600

    Add Prettier configuration and scripts; update ESLint config for compatibility

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "object-curly-newline": [
+        "error",
+        {
+          ObjectExpression: { minProperties: 1, multiline: true },
+          ObjectPattern: { minProperties: 3, multiline: true },
+          ImportDeclaration: { minProperties: 3, multiline: true },
+          ExportDeclaration: { minProperties: 3, multiline: true },
+        },
+      ],
+    },
+  },
+  ...compat.extends("prettier"),
   {
     ignores: [
       "node_modules/**",
@@ -20,6 +34,6 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
