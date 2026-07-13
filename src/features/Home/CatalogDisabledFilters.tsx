40a61e08bad114f9339416d3759f2b62f0c48016
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-07-13 01:03:05 -0600

    feat: add "use client" directive to CatalogDisabledFilters component

--- src/features/Home/CatalogDisabledFilters.tsx
+++ src/features/Home/CatalogDisabledFilters.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { DropdownBrands } from "@/features/ProductListing/DropdownBrands"
 import { DropdownCategories } from "@/features/ProductListing/DropdownCategories"
 import { SearchInput } from "@/features/ProductListing/SearchInput"
